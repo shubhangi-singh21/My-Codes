7e4224c8e36050169814d777fd7651276cc6c3a2
Shuhangi Singh <shubhangifas@gmail.com>
2020-08-15 00:59:36 +0530

    update

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,24 +1,42 @@
-#include <iostream.h>
+#include <iostream>
 using namespace std;
 
-  int BinSearch (int A, int n, int kay)
+  int BinSearch (int A[], int n, int l, int h, int key)
   {
-    int l = 1;
-    int h = n;
+    int mid;
     while (l < h)
-    { 
-      mid = (l + h)/ 2;
+    { mid = ( l + h )/2;
       if (key == A [mid])
       {
         return mid;
-       }
+      }
        else if (key < A [mid])
        {
          h = mid - 1;
+         {return BinSearch (A, n, l, mid-1, key);}
         }
         else
         {
           l = mid + 1;
-         }
+          {return BinSearch (A, n, mid+1, h, key);}
+        }
       }
-  
+      return -1;
+  }
+   int main ()
+   {
+       int A[] = { 2, 3, 4, 10, 40 }; 
+    int key = 4; 
+    int n = sizeof(A) / sizeof(A[0]); 
+    int l = 1;
+    int h = n;
+    int mid = ( l + h )/2;
+    int result = BinSearch(A, n, l, h, key); 
+    (result == -1) ? cout << "Element is not present in array"
+                   : cout << "Element is present at index " << result; 
+    if (result == mid)
+    {
+        cout << "Element is present at index " << result;
+    }
+    return 0; 
+   }
