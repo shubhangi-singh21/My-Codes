ffb8e39c217ec9d37fbcedd5ad6b3664cc449971
Shuhangi Singh <shubhangifas@gmail.com>
2020-08-15 23:06:15 +0530

    create new file

---
+++ linear_search.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+    int LinSearch(int A [], int key)
+   {    int i;
+        for (i = 0; i< 5; i ++)
+        {
+            if ( key == A [i] )
+                {
+                    cout << "Element found at index "<< i+1;
+                    break;
+                }
+        }
+          cout << "Element not found";
+    return -1;
+    }
+
+int main()
+{
+  int A[10], key, i;
+  for (i= 0; i< 5; i ++)
+  {
+    cin >> A[i];
+   }
+   
+   cin >> key;
+   return LinSearch(A, key);
+   
+}
