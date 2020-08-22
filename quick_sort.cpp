d9e35a6b24253c21452e8ffac21563e36d3bfd47
Shuhangi Singh <shubhangifas@gmail.com>
2020-08-22 23:53:47 +0530

    added partition algo

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,1 +1,22 @@
-
+Partion ( l, h )
+{
+  pivot = A [ l ];
+  i = l;
+  j = h;
+  while ( i < j )
+  {
+    do
+    {
+      i ++;
+    } while ( A [ i ] <= pivot );
+    do
+    {
+      j --;
+    } while ( A [ j ] > pivot);
+    if ( i < j )
+      swap ( A [ i ], A [ j ] );
+  }
+  swap ( A [ l ], A [ j ] );
+  return j;
+}
+   
