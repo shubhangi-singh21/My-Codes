39b774bd9ddfdc99fbded72ca169422daeb05201
Shuhangi Singh <shubhangifas@gmail.com>
2020-09-29 19:51:09 +0530

    small fixes

--- parenthesis_using_graph.cpp
+++ parenthesis_using_graph.cpp
@@ -26,20 +26,20 @@ vector<string>removeInvalidParenthesis (string s) {
 		if(isBalanced(u)) found=1;
 		ans.push_back(u);
 		if(found) continue;
-		for(int i=0; i<u.size;i++)
+		for(int i=0; i<u.size();i++){
 			if(u[i]=='(' || u[i]==')')
 			{
-				string v=u.substr(0,i) + u.substr(i+1 , u.size()));
+				string v=u.substr(0,i) + u.substr(i+1 , u.size());
 				Q.push(v);
 			}
 		}
 	}
 	return ans;
-}
+};
 };
 int main() {
 Solution sol;
-vector<string>result= s.removeInvalidParenthesis("((a)dwdj)");
+vector<string>result= sol.removeInvalidParenthesis("((a)dwdj)");
 for(string cur :result) cout<<cur<<endl;
 return 0;
 }
