c7c6deba7aebdbc83abcbab9afe6997463c9ad54
Shuhangi Singh <shubhangifas@gmail.com>
2020-09-29 20:05:01 +0530

    added problem statement

--- zigzag_iterator.cpp
+++ zigzag_iterator.cpp
@@ -1,3 +1,14 @@
+//Given two 1d vectors, implement an iterator to return their elements alternately.
+//v1 = [1, 2]
+//v2 = [3, 4, 5, 6]
+//By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1, 3, 2, 4, 5, 6].
+
+For example, given two 1d vectors:
+
+v1 = [1, 2]
+v2 = [3, 4, 5, 6]
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1, 3, 2, 4, 5, 6].
+
 class ZigzagIterator {
 public:
     ZigzagIterator(vector<int>& v1, vector<int>& v2) {
