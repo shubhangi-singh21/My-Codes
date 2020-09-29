75d70403100fb79f33b3363a22c2468c5ddb663c
Shuhangi Singh <shubhangifas@gmail.com>
2020-09-29 18:17:20 +0530

    fixed syntax errors

--- balance_tree.cpp
+++ balance_tree.cpp
@@ -5,16 +5,18 @@ int checkHeight(TreeNode root) {
   if (root == null) return -1;
 
  int leftHeight = checkHeight(root.left);
- if (leftHeight Integer.MIN_VALUE) return Integer.MIN_VALUE; // Pass error up
+ if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Pass error up
 
  int rightHeight checkHeight(root.right);
  if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Pass error up
 
  int heightDiff = leftHeight - rightHeight;
- if (Math.abs(heightDiff) > 1) {
+ if (abs(heightDiff) > 1) {
  return Integer.MIN_VALUE; // Found error -> pass it back
- } else {
- return Math.max(leftHeight, rightHeight) + 1;
+ } 
+  else 
+ {
+ return max(leftHeight, rightHeight) + 1;
  }
  }
  boolean isBalanced(TreeNode root) {
