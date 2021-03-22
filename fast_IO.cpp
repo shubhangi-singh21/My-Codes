0a0469aa4240a73f59d2be83181e1ba1bd98ded3
Shubhangi Singh <shubhangifas@gmail.com>
2021-03-23 01:31:56 +0530

    Create fast_IO.cpp

---
+++ fast_IO.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fastIO(){
+    
+    ios_base::sync_with_stdio(false); //this truns off syncrozination between C++ and C standard streams
+    cin.tie(NULL); //unties input output streams
+
+    
+}
+
+int main() {
+	
+    fastIO();
+    
+    int n;
+    cin>>n;
+    cout<<n<<"\n";
+	
+	return 0;
+}
