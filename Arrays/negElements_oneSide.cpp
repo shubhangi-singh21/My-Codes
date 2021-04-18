3f11fea9c0ae3ca32bb7e31dfe197363ea9476b8
Shubhangi Singh <shubhangifas@gmail.com>
2021-04-18 23:39:02 +0530

    Create negElements_oneSide.cpp

---
+++ Arrays/negElements_oneSide.cpp
@@ -0,0 +1,35 @@
+//An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void rearrange(int arr[], int n)
+{
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < 0) {
+            if (i != j)
+                swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+}
+ 
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+}
+ 
+int main()
+{
+    int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    rearrange(arr, n);
+    printArray(arr, n);
+    return 0;
+}
+
+//Output: -1 -3 -7 4 5 6 2 8 9
+//Time complexity: O(N) 
+//Auxiliary Space: O(1)
