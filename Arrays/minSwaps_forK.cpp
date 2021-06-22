2be8aa07af855ffb78502825a06c8bcf8715ec32
Shubhangi Singh <shubhangifas@gmail.com>
2021-06-22 18:52:17 +0530

    Update minSwaps_forK.cpp

--- Arrays/minSwaps_forK.cpp
+++ Arrays/minSwaps_forK.cpp
@@ -7,58 +7,51 @@
 //Auxiliary space: O(1)
 
 #include <iostream>
+
 using namespace std;
-  
-// Utility function to find minimum swaps
-// required to club all elements less than
-// or equals to k together
-int minSwap(int *arr, int n, int k) {
-      
-    // Find count of elements which are
-    // less than equals to k
-    int count = 0;
-    for (int i = 0; i < n; ++i)
-        if (arr[i] <= k)
-            ++count;
-      
-    // Find unwanted elements in current
-    // window of size 'count'
-    int bad = 0;
-    for (int i = 0; i < count; ++i)
-        if (arr[i] > k)
-            ++bad;
-      
-    // Initialize answer with 'bad' value of
-    // current window
-    int ans = bad;
-    for (int i = 0, j = count; j < n; ++i, ++j) {
-          
-        // Decrement count of previous window
-        if (arr[i] > k)
-            --bad;
-          
-        // Increment count of current window
-        if (arr[j] > k)
-            ++bad;
-          
-        // Update ans if count of 'bad'
-        // is less in current window
-        ans = min(ans, bad);
+
+int minswap(int arr[], int n, int k){
+    int count=0, i,j;
+    for(i=0; i<n; i++){
+        if(arr[i]<=k){
+            count++;
+        }
+    }
+    int bad=0;
+    for(i=0; i<count;i++){
+        if(arr[i]>k){
+            bad++;
+        }
+    }
+    
+    int ans=bad;
+    for(i=0, j<count; j<n; i++, j++){
+        if(arr[i]>k){
+            bad--;
+        }
+        
+        if(arr[j]>k){
+            bad++;
+        }
+    ans=min(ans, bad);
     }
     return ans;
 }
-  
-// Driver code
-int main() {
-      
-    int arr[] = {2, 1, 5, 6, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int k = 3;
-    cout << minSwap(arr, n, k) << "\n";
-      
-    int arr1[] = {2, 7, 9, 5, 8, 7, 4};
-    n = sizeof(arr1) / sizeof(arr1[0]);
-    k = 5;
-    cout << minSwap(arr1, n, k);
+
+int main()
+{
+    int n, k, i,arr[20];
+    cout<<"Enter number of elements in the array:";
+    cin>>n;
+    cout<<endl;
+    cout<<"Enter the elements of the array:";
+    for(i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+    cout<<"Enter the k element:";
+    cin>>k;
+    cout<<endl;
+    cout<< minswap(arr, n, k);
     return 0;
 }
