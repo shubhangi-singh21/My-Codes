5dfd9acf03b41051d0e632417b1426a4d3b308ac
Shubhangi Singh <shubhangifas@gmail.com>
2021-07-07 17:27:30 +0530

    Update bipartite_DFS.cpp

--- Graph/bipartite_DFS.cpp
+++ Graph/bipartite_DFS.cpp
@@ -1,17 +1,3 @@
-bool checkBipartite( vector <int> adj[], int n)
-{
-  int color[n];
-  memset( color, -1, sizeof(color));
-  for( i = 0; i < n; i++)
-  {
-    if( color[i] == -1)
-    {
-      color[i] = 1;
-      if( !bipartiteDFS( i, adj, color)
-         return false;
-    }
-  }
-    return true;
          
 bool bipartiteDFS( int node, vector <int> adj[], int color[])
          {
@@ -28,4 +14,42 @@ bool bipartiteDFS( int node, vector <int> adj[], int color[])
              
              return true;
          }
+
+  
+bool checkBipartite( vector <int> adj[], int n)
+{
+  int color[n];
+  memset( color, -1, sizeof(color));
+  for( i = 0; i < n; i++)
+  {
+    if( color[i] == -1)
+    {
+      color[i] = 1;
+      if( !bipartiteDFS( i, adj, color)
+         return false;
+    }
+  }
+    return true;
+}
+         
+           
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  vector <int> adj[n];
+  for( i = 0; i < m; i++)
+  {
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  
+  if(checkBipartite(adj, n))
+    cout << "Yes";
+  else
+    cout << "No";
+  return 0;
+}
          
