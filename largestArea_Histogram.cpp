14790e4b5d36b72b80aac16bd3f0e9cdad255aa6
Shubhangi Singh <shubhangifas@gmail.com>
2021-07-25 14:01:15 +0530

    optimized solution

--- largestArea_Histogram.cpp
+++ largestArea_Histogram.cpp
@@ -1,13 +1,51 @@
+/*Leetcode #84 
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+*/
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        std::sort(heights.begin(), heights.end(), greater<int>());
-        if(heights.size()==1)
-                return heights[0];
-        else if(heights[1]==0)
-            return heights[0];
-        else 
-            return heights[1]*2;
-        return 0;
+        int n = heights.size();
+        vector<int> left(n), right(n);
+        
+        stack<int> mystack;
+        for( int i=0; i<n; ++i) //Filling left side limit values
+        {
+            if(mystack.empty())
+            {
+                left[i] = 0;
+                mystack.push(i);
+            }
+                else{
+                    while(!mystack.empty() and heights[mystack.top()]>=heights[i])
+                        mystack.pop();
+                    left[i] = mystack.empty()?0:mystack.top()+1;
+                    mystack.push(i);
+                }
+        }
+        while(!mystack.empty()) //Clearing stack, reusing it
+            mystack.pop();
+        
+        for( int i=n-1; i>=0; --i) //Filling right side limit values
+        {
+            if(mystack.empty())
+            {
+                right[i] = n-1;
+                mystack.push(i);
+            }
+            else
+            {
+                while(!mystack.empty() and heights[mystack.top()]>=heights[i])
+                        mystack.pop();
+                    right[i] = mystack.empty()?n-1:mystack.top()-1;
+                    mystack.push(i);
+            }
+                
+        }
+        
+        int mx_area=0; //stores max area
+        for( int i = 0; i<n; ++i)
+            mx_area= max(mx_area, heights[i]*(right[i]-left[i]+1));
+        return mx_area;
     }
 };
